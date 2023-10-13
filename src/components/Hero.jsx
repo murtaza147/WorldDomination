09b2ef08e0de95286988289d059618ab8f1184a4
luxman17 <94662885+luxman17@users.noreply.github.com>
2023-10-13 16:47:53 -0400

    hero ux
    background is black

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-    
+import '../index.css';
 function Hero(){
     return(
-        <a>Hero</a>
+        <div className="rectangle-container">
+            <div className="rectangle">
+                <div className="text-center">
+                        <div className="montserratFont">
+                            <h2>Welcome To</h2>
+                            <h3>Stocia</h3>
+                            <p>Mental Health and Well Being</p>
+                        </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
