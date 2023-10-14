a335f0d31cafa0988503f699c71a0ffd5ceb4f20
luxman17 <94662885+luxman17@users.noreply.github.com>
2023-10-14 19:40:32 -0400

    lux week 1 done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import '../index.css';
-function Hero(){
-    return(
+import androidImage from './android.jpg';
+import iosImage from './ios.jpg';
+import StoicaImage from './stoica.jpg';
+import calm from './calm.jpg';
+function Hero() {
+    return (
         <div className="rectangle-container">
             <div className="rectangle">
-                <div className="text-center">
-                        <div className="montserratFont">
-                            <h2>Welcome To</h2>
-                            <h3>Stocia</h3>
-                            <p>Mental Health and Well Being</p>
-                            <div className="inner-rectangle">
-
+            <div className="text-center">
+                        <h2 class="montserratFont">Welcome to</h2>
+                        <h2 class="stitle">S T O I C A</h2>
+                        <h3 class ="montserratFont">MENTAL HEALTH & WELL BEING</h3>
+                        
+                                
+                        <div className="inner-rectangle">
+                            <div className="inner-rectangle2">
+                                <img src={calm} alt="ios" className="first-image"/>
+                                <div className="button-container">
+                                    <button className="button2">Download app on iOS</button>
+                                    <button className="button1">Download app on Android</button>
+                                    <img src={iosImage}className="third-image"/>
+                                    <img src={androidImage}className="second-image"/>
+                                    
+                                </div>
+                                
                             </div>
-                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-    )
+        
+    );
 }
 
-export default Hero
+export default Hero;
+
+
+
+
